9fe5be844f1245c22245be63901d33d1fb0fabbe
lilpilot <master@lilpilot.co>
2017-05-01 17:42:50 +0800

    init project

---
+++ src/main/java/co/lilpilot/ZeroColaApplication.java
@@ -0,0 +1,12 @@
+package co.lilpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroColaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroColaApplication.class, args);
+	}
+}
