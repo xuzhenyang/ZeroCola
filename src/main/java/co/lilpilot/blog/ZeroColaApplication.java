5a36ac21cfd3b53ad57c8b2a7a40cce10a9f8a84
lilpilot <master@lilpilot.co>
2017-07-23 20:54:54 +0800

    use devtools to reload app

--- src/main/java/co/lilpilot/blog/ZeroColaApplication.java
+++ src/main/java/co/lilpilot/blog/ZeroColaApplication.java
@@ -2,17 +2,9 @@ package co.lilpilot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZeroColaApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ZeroColaApplication.class);
-	}
-
+public class ZeroColaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZeroColaApplication.class, args);
 	}
