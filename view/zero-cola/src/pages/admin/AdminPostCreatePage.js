ba74bb16fd37289c298e2ed467ce677f46f37f5d
徐祯阳 <xuzhenyang@souche.com>
2017-12-23 14:32:12 +0800

    create post

--- view/zero-cola/src/pages/admin/AdminPostCreatePage.js
+++ view/zero-cola/src/pages/admin/AdminPostCreatePage.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
 import AdminLayout from '../../components/AdminLayout';
-import { Form, Input, Button, Select, Tabs } from 'antd';
+import { Form, Input, Button, Select, Tabs, message } from 'antd';
+import marked3 from 'marked3';
+import { request } from '../../common';
+import { tokenKey } from '../../config';
 const FormItem = Form.Item;
 const { TextArea } = Input;
 const TabPane = Tabs.TabPane;
 
 class AdminPostCreatePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: ''
+        }
+        this.onContentChange = this.onContentChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onContentChange(e) {
+        this.setState({
+            content: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { validateFields, resetFields } = this.props.form;
+        validateFields((errors, values) => {
+            if (errors) {
+                return;
+            }
+            const token = window.localStorage.getItem(tokenKey);
+            const data = {
+                title: values.title,
+                tags: values.tags,
+                content: values.content,
+            };
+            request('/api/v1/admin/posts', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": `${token}`
+                },
+                body: JSON.stringify(data)
+            }).then((response) => {
+                this.props.history.push('/admin/posts');
+                resetFields();
+            }).catch((error) => {
+                message.error('submit failed');
+            })
+        });
+    }
+
     render() {
 
+        const { getFieldDecorator } = this.props.form;
+
         const formItemLayout = {
-            labelCol: { span: 1 },
+            labelCol: { span: 2 },
             wrapperCol: { span: 8 }
         }
 
@@ -19,23 +69,33 @@ class AdminPostCreatePage extends Component {
                     <h1>Admin Post Create Page</h1>
                     <Form>
                         <FormItem label="Title" {...formItemLayout}>
-                            <Input />
+                            {getFieldDecorator('title', {
+                                rules: [{ required: true, message: 'please input title !' }]
+                            })(
+                                <Input />
+                                )}
                         </FormItem>
-                        <FormItem label="Tag" {...formItemLayout}>
-                            <Select />
+                        <FormItem label="Tags" {...formItemLayout}>
+                            {getFieldDecorator('tags')(
+                                <Select />
+                            )}
                         </FormItem>
                         <Tabs defaultActiveKey="1">
                             <TabPane tab="edit" key="1">
                                 <FormItem>
-                                    <TextArea rows={20} />
+                                    {getFieldDecorator('content', {
+                                        rules: [{ required: true, message: 'please input content !' }]
+                                    })(
+                                        <TextArea rows={20} onChange={this.onContentChange} />
+                                    )}
                                 </FormItem>
                             </TabPane>
                             <TabPane tab="preview" key="2">
-                                <TextArea readOnly={true} rows={20} />
+                                <div style={{height: 420, overflowY: "scroll", marginBottom: 24, border: "1px solid #d9d9d9", borderRadius: 4 }} dangerouslySetInnerHTML={{ __html: marked3(this.state.content) }} />
                             </TabPane>
                         </Tabs>
                         <Button>Draft</Button>
-                        <Button>Submit</Button>
+                        <Button onClick={this.handleSubmit}>Submit</Button>
                     </Form>
                 </AdminLayout>
             </div>
